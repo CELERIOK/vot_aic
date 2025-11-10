6420e7ebac45ff64c7806519b62d9b649da5c23d
CELERIOK <240529402+CELERIOK@users.noreply.github.com>
2025-11-10 21:26:01 +0000

    Upload the same project without the base directory

---
+++ src/Components/Description.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Description: React.FC = () => {
+  return (
+    <section id="description" className="py-20 px-6 bg-white">
+      <div className="max-w-3xl mx-auto text-center">
+        <p className="text-lg md:text-xl text-gray-700 leading-relaxed">
+          En VOT, ofrecemos soluciones integrales y personalizadas para la gestión fiscal, contable, legal y laboral de su empresa. Nuestro equipo de expertos está comprometido con su éxito, brindándole la tranquilidad y el soporte que necesita para crecer en el mercado canario.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Description;
---
+++ src/services/email.ts
@@ -0,0 +1,5 @@
+export const EMAIL_CONFIG = {
+    TU_SERVICE_ID: "service_wb8ngfw",
+    TU_TEMPLATE_ID : "template_s1zqj78",
+    TU_PUBLIC_KEY: "4iSOnu-5UhqNKGKJ6"
+}
